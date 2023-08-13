885ceec7476de92a437110215ae3f7cf4aec95fe
MykhailoLoniak <139628544+MykhailoLoniak@users.noreply.github.com>
2023-08-13 20:35:36 +0200

    Create arrList.js

---
+++ src/components/arrList.js
@@ -0,0 +1,40 @@
+const arrList = [
+  {
+    title: 'HitFM_Ukr',
+    img: './img/HitFM.png',
+    audioSrc: 'http://online.hitfm.ua/HitFM_Ukr',
+  },
+  {
+    title: 'HitFM_Ukr_Live',
+    img: './img/HitFM.png',
+    audioSrc: 'http://online.hitfm.ua/HitFM_Ukr_Live',
+  },
+
+  {
+    title: 'Львівська_хвиля',
+    img: './img/lfm.png',
+    audioSrc: 'https://onair.lviv.fm:8443/lviv32.fm',
+  },
+  {
+    title: 'KissFM_Ukr',
+    img: './img/KissFM_player_logo.jpg',
+    audioSrc: 'http://online.kissfm.ua/KissFM_Ukr_HD',
+  },
+  {
+    title: 'Наше_Радіо_Live',
+    img: './img/channels4_profile.jpg',
+    audioSrc: 'http://online.nasheradio.ua/NasheRadio_Live',
+  },
+  {
+    title: 'Український_рок',
+    img: './img/RadioROKS_UARock_1200x630_0918.jpg',
+    audioSrc: 'http://music.myradio.ua:8000/PopRock192.mp3',
+  },
+  {
+    title: 'Fm_Галичина',
+    img: './img/HitFM.png',
+    audioSrc: 'https://stream320.galychyna.fm/WebSite',
+  },
+];
+
+export default arrList;
